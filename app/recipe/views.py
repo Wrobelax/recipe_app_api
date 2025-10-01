fa1c21eb184d257a6e9c5e9c267476fe3d03a46d
Adrian <awrobel.wr@gmail.com>
2025-10-01 16:42:33 +0200

    Resolved linting issues.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 """
 Views for the recipe APIs.
 """
-from drf_spectacular.utils import(
+from drf_spectacular.utils import (
     extend_schema_view,
     extend_schema,
     OpenApiParameter,
@@ -92,6 +92,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @extend_schema_view(
     list=extend_schema(
